cba92ca63946ad212dd12d31a0870d14bba10ff4
Anton <noten.b@hotmail.com>
2020-06-30 00:43:23 +0200

    Fixed namespace issue

--- Common/Builder.cs
+++ Common/Builder.cs
@@ -11,7 +11,6 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using Common;
 
 namespace MonogameTexturePacker {
     class Builder {
--- Common/CodeBuilder.cs
+++ Common/CodeBuilder.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Common {
+namespace MonogameTexturePacker {
     class CodeBuilder {
         private StringBuilder builder;
         private int indent;
